38115afaa709831425af18a7c807cab072029c78
elbek <elbekkulmurodov01@gmail.com>
2024-09-14 17:08:12 +0500

    cicd yaml file

--- apps/models.py
+++ apps/models.py
@@ -1,3 +1,12 @@
-from django.db import models
+from django.db.models import Model, ForeignKey, CASCADE
+from django.forms import CharField
+
 
 # Create your models here.
+
+class Category(Model):
+   name = CharField(max_length=155)
+
+class Product(Model):
+   name = CharField(max_length=155)
+   category = ForeignKey('apps.Category', CASCADE)
--- apps/urls.py
+++ apps/urls.py
@@ -3,6 +3,4 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('apps.urls'))
 ]
